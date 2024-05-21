1c33f23b977f1f53e8c5a71017e007b9b11192b8
Matt2130 <manuel.hdz.rubio07@gmail.com>
2024-05-21 07:49:44 -0600

    Programa 9 - Contraseña

---
+++ ejercicio9.py
@@ -0,0 +1,7 @@
+while True:
+    contraseña = input("Ingrse su contraseña: ")
+    if contraseña == "POO123":
+        print("Bienvenido al programa")
+        break
+    else:
+        print("Contraseña incorrecta")
