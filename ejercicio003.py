b09355add7f14efbe97bbcebb706045097ff85a7
Matt2130 <manuel.hdz.rubio07@gmail.com>
2024-06-05 07:26:36 -0600

    Programa 3 - IMC

---
+++ ejercicio003.py
@@ -0,0 +1,5 @@
+peso = float(input("Ingrese su peso (en kg): "))
+estatura = float(input("Ingrese su estatura (en metros): "))
+imc = (peso) / (estatura**2)
+imc_round = round(imc,2)
+print(f"Tu índice de masa corporal es de {imc_round}")
