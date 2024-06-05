399fc9160a5c551a67b473bacaadc321c3948c17
Matt2130 <manuel.hdz.rubio07@gmail.com>
2024-06-05 07:32:44 -0600

    Programa 15 - For/if con lista de edades

---
+++ ejercicio015.py
@@ -0,0 +1,14 @@
+edades = [2,5,76,8,10,58,63,14,18,20,75,63,9]
+may_edad = []
+men_edad = []
+
+print(f"\nLa lista con todas las edades es {edades}")
+
+for i in edades:
+    if i <= 17: 
+        men_edad.append(i)
+    else:
+        may_edad.append(i)
+        
+print(f"\nLa lista con las edades menor de edad es {men_edad}")
+print(f"La lista con las edades mayor de edad es {may_edad}\n")
